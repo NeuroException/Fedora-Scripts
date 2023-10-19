157535a9de982d8d503da8ba8817d3d1bda5d4d2
Neuro <118199015+NeuroException@users.noreply.github.com>
2023-10-19 12:41:20 +0200

    Update awesomewm.sh
    Added luarocks to dependencies

--- awesomewm.sh
+++ awesomewm.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/awesomewm/awesome && cd awesome
 
 # Install build dependencies
-sudo dnf install -y cmake lua asciidoctor lua-ldoc-doc lua-devel libX11-xcb libX11-xcb xcb-util libxcb-devel xcb-util-cursor xcb-util-cursor-devel cairo cairo-devel xcb-util-devel xcb-util xcb-util-keysyms xcb-util-keysyms-devel xcb-util-xrm xcb-util-xrm-devel libxkbcommon-devel libxdg-basedir-devel startup-notification gdk-pixbuf2-devel libxkbcommon-x11-devel xcb-util-xrm-devel xcb-util-wm xcb-util-wm-devel startup-notification libnotify-devel pkgconfig startup-notification-devel dbus-devel libxcb-devel luarocks rpm-build rpm-build lua-lgi
+sudo dnf install -y cmake lua asciidoctor lua-ldoc-doc lua-devel libX11-xcb libX11-xcb xcb-util libxcb-devel xcb-util-cursor xcb-util-cursor-devel cairo cairo-devel xcb-util-devel xcb-util xcb-util-keysyms xcb-util-keysyms-devel xcb-util-xrm xcb-util-xrm-devel libxkbcommon-devel libxdg-basedir-devel startup-notification gdk-pixbuf2-devel libxkbcommon-x11-devel xcb-util-xrm-devel xcb-util-wm xcb-util-wm-devel startup-notification libnotify-devel pkgconfig startup-notification-devel dbus-devel libxcb-devel luarocks rpm-build rpm-build lua-lgi luarocks
 
 # Compile
 make package
