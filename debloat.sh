e817612604b377b254dc64953142489b52eecce1
Neuro <118199015+NeuroException@users.noreply.github.com>
2023-10-19 11:19:29 +0200

    Add debloat.sh
    A script for debloating Fedora.

---
+++ debloat.sh
@@ -0,0 +1,31 @@
+dnf remove -y gnome-connections \
+	anaconda \
+	gnome-characters \
+	gnome-classic-session \
+	gnome-contacts \
+	gnome-maps \
+	gnome-photos \
+	gnome-remote-desktop \
+	gnome-system-monitor \
+	gnome-tour \
+	gnome-user-docs \
+	yelp \
+	cheese \
+	rhythmbox \
+	totem \
+	libreoffice-core \
+	gnome-boxes \
+	unoconv \
+	gnome-weather \
+	gnome-clocks \
+	gnome-calculator \
+	simple-scan \
+	mediawriter \
+	gnome-font-viewer \
+	evince \
+	gnome-disk-utility \
+	baobab \
+	gnome-font-viewer \
+	eog \
+	gnome-calendar \
+	gnome-terminal
